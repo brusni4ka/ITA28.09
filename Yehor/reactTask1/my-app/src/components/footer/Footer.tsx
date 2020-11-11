77bb8a0d0c72fde1224c8fe90f40063abc504dd0
Yehor <yehorkuch@gmail.com>
2020-11-11 19:26:23 +0200

    netflix project

---
+++ Yehor/reactTask1/my-app/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+    <footer>
+        <h3>netflixroulette</h3>
+    </footer>)
+
+}
+
+export default Footer;
