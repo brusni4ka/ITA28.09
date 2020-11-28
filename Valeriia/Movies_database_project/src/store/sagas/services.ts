285022bde3635994e6b339c80f6001ade31ef4e9
Valeriia-Rez <Valeria09122016@gmail.com>
2020-11-28 16:25:53 +0200

    Remove space to avoid an issue with search by title URL

--- Valeriia/Movies_database_project/src/store/sagas/services.ts
+++ Valeriia/Movies_database_project/src/store/sagas/services.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { IMovie } from "../../types";
 
 export const fetchListOfMoviesBySortType = async(sortByType: string, searchBy?: string, searchValue?: string): Promise<[]> => {
-    let url = searchBy && searchValue ? `https://reactjs-cdp.herokuapp.com/movies?sortBy=${sortByType}&searchBy=
-    ${searchBy}&sortOrder=desc&${searchBy === "title" ? `search=${searchValue}` : `filter=${searchValue}`}&limit=20`
+    console.log(searchValue)
+    let url = searchBy && searchValue ? `https://reactjs-cdp.herokuapp.com/movies?sortBy=${sortByType}&${searchBy==="title" ?`search=${searchValue}`:`filter=${searchValue}`}&searchBy=${searchBy}&sortOrder=desc&limit=20`
      : `https://reactjs-cdp.herokuapp.com/movies?sortBy=${sortByType}&sortOrder=desc&limit=20`;
     const result = await axios(url); 
     return result.data.data;
