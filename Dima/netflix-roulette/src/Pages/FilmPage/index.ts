4e16e8ddeec91920942b3c5b8190ebeef8e64b58
MadM0ose <58383937+MadM0ose@users.noreply.github.com>
2020-12-04 14:13:38 +0200

    Routing task (#23)
    * murkup for VanGoux
    
    * added changes in some styles
    
    * first react task
    
    * first task
    
    * add routing
    
    * routing task and refactoring
    
    * redux tusk with requests
    
    * refactoring in requests logic
    
    * hot fixes in search input
    
    * add pagination and resolve all issues
    
    Co-authored-by: MadM0ose <revaz.molotov@gmail.com>

---
+++ Dima/netflix-roulette/src/Pages/FilmPage/index.ts
@@ -0,0 +1,23 @@
+import FilmPage from './FilmPage'
+
+// import { currentFilmRequested, filmsRequested } from '../../redux/Actions/requestActions'
+// import { connect, ConnectedProps} from 'react-redux';
+// import { IRootState } from 'interfaces/IRootState';
+// import { RouteComponentProps } from 'react-router-dom';
+
+
+// const mapStateToProps = (state: IRootState) => ({
+//   films: state.films.films,
+//   currentFilm: state.films.currentFilm,
+//   id: state.id
+// });
+// const mapDispatchToProps = {
+//   currentFilmRequested,
+//   filmsRequested,
+// }
+
+// const connector = connect(mapStateToProps, mapDispatchToProps)
+
+// export type PropsFromRouteAndRedux = ConnectedProps<typeof connector> & RouteComponentProps<{ id: string }>
+
+// export default connector(FilmPage)
